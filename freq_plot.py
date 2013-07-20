e4b4527778bd0b7ad9dc5622694e4cb5de4f8c00
Hackbright student <user@mini-005.(none)>
2013-07-19 18:03:37 -0700

    sampling

--- freq_plot.py
+++ freq_plot.py
@@ -9,7 +9,9 @@ print "RATE: ", rate
 print "DATA: ", data[10000:20000]  # taking sample of track
 print "DATA LEN: ", len(data)
 
-data_test = data[:(rate*20)]
+num_samp_sec = 10
+num_samp = 2
+data_test = data[:(rate*num_samp_sec*2)]
 print "DATA TEST LEN: ", len(data_test)
 
 data_lr_avg = []
@@ -21,49 +23,60 @@ for i in range(len(data_test)):
 print "DATA LR AVG LEN: ", len(data_lr_avg)
 
 d = {}
-num_samp_sec = 10
 
-# sample_dict = {}
 
-# for i in range(0, len(data_lr_avg)/rate/num_samp_sec, 1):
-#     sample_dict[i] = None
+section_dict = {}
 
-# for j in range(len(sample_dict)):
+for i in range(0, len(data_lr_avg)/rate/num_samp_sec, 1):
+    section_dict[i] = None
+
+# for j in range(len(section_dict)):
 #     for k in range(num_samp_sec):
-#             sample_dict[j] = data_lr_avg[k:k+num_samp_sec]
+#             section_dict[j] = data_lr_avg[k:k+num_samp_sec]
 
 # # for i in range(0, len(data_lr_avg), rate/num_samp_sec):
-# #     sample_dict[i] = data_lr_avg[i:i+num_samp_sec]
+# #     section_dict[i] = data_lr_avg[i:i+num_samp_sec]
 
-# print sample_dict
+print "EMPTY DICT:", section_dict
+print "LEN SECTION DICT: ", len(section_dict)
+
+i = 0
+for j in range(0, len(data_lr_avg), rate*num_samp_sec):
+    section_dict[i] = data_lr_avg[j:j+(rate*num_samp_sec)]
+    i += 1
 
  # ===========================================
-for i in range(0, 1):  # WHOLE SONG # i is sample number, s1, s2, etc.
+for i in range(0, len(section_dict)):  # WHOLE SONG # i is sample number, s1, s2, etc.
     sample = []
-    # get first sample, s1 (one second)
-    for j in range(0, rate, (rate/num_samp_sec)):
-        sample.append(data_lr_avg[j])
+    # print len(section_dict[i])
+    for j in range(0, len(section_dict[i]), (rate/num_samp_sec)):
+        # print section_dict[i][j]
+        added_elements = section_dict[i][j:j+(rate/num_samp_sec)]
+        print len(added_elements)
+        # sample_avg = sum(added_elements)/len(added_elements)
+        # print sample_avg
+        # sample.append(section_dict[i][j])
 
     print "SAMPLE LENGTH:", len(sample), "SHOULD EQUAL", num_samp_sec
 
-    fft_data = np.fft.fft(sample)  # Compute the one-dimensional discrete Fourier Transform. (Returns complex values)
-    print "FFT DATA:", fft_data
+#     fft_data = np.fft.fft(sample)  # Compute the one-dimensional discrete Fourier Transform. (Returns complex values)
+#     print "FFT DATA:", fft_data
 
-    fft_abs = abs(fft_data)  # Taking abs value to get distance from zero (amplitude of frequency)
-    print "FFT ABS: ", fft_abs
-    max_fft_abs = max(fft_abs)
-    print "MAX FFT ABS:", max(fft_abs)
+#     fft_abs = abs(fft_data)  # Taking abs value to get distance from zero (amplitude of frequency)
+#     print "FFT ABS: ", fft_abs
+#     max_fft_abs = max(fft_abs)
+#     print "MAX FFT ABS:", max(fft_abs)
 
-    dict_key = max_fft_abs
-    print "DICT KEY: ", dict_key
+#     dict_key = max_fft_abs
+#     print "DICT KEY: ", dict_key
 
-    if dict_key in d.keys() is not None:   # if dict key exists, append
-        d[dict_key].append(i)  # k is frequency(key) and i is sample number(value)
-    else:  # else create dict key and assign value
-        d[dict_key] = i
+#     if dict_key in d.keys() is not None:   # if dict key exists, append
+#         d[dict_key].append(i)  # k is frequency(key) and i is sample number(value)
+#     else:  # else create dict key and assign value
+#         d[dict_key] = i
 
 
-print "DICTIONARY: ", d
+# print "DICTIONARY: ", d
 
  # ===========================================
 
