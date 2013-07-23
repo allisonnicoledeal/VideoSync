65fd52e537edc9c4861ddd824d3ff52f438726fb
Hackbright student <user@mini-005.(none)>
2013-07-22 17:00:15 -0700

    Added process by second fxns

--- freq_plot.py
+++ freq_plot.py
@@ -1,129 +1,101 @@
-import numpy as np
 import scipy.io.wavfile
-import matplotlib.pyplot as plt
-# from pylab import *
-from scipy import *
-
-rate, data = scipy.io.wavfile.read('Hiphopopotamus.wav')  # Return the sample rate (in samples/sec) and data from a WAV file
-print "RATE: ", rate
-print "DATA: ", data[10000:20000]  # taking sample of track
-print "DATA LEN: ", len(data)
-
-num_samp_sec = 10
-num_samp = 2
-data_test = data[:(rate * num_samp)]
-print "DATA TEST LEN: ", len(data_test)
-
-data_lr_avg = []
-for i in range(len(data_test)):
-    average = (data_test[i][0] + data_test[i][1]) / 2
-    data_lr_avg.append(average)
-
-print "DATA LR AVG LEN: ", len(data_lr_avg)
-
-d = {}
+import numpy as np
 
 
-section_dict = {}
+sample_rate = 44100
 
-for i in range(0, len(data_lr_avg)/rate, 1):
-    section_dict[i] = None
+# Read file
+# INPUT: Audio file
+# OUTPUT: Sets sample rate of wav file, Returns data read from wav file (numpy array of integers)
+def read_audio(file):
+    rate, data = scipy.io.wavfile.read(file)  # Return the sample rate (in samples/sec) and data from a WAV file
+    sample_rate = rate
+    return data
 
-# for j in range(len(section_dict)):
-#     for k in range(num_samp_sec):
-#             section_dict[j] = data_lr_avg[k:k+num_samp_sec]
 
-# # for i in range(0, len(data_lr_avg), rate/num_samp_sec):
-# #     section_dict[i] = data_lr_avg[i:i+num_samp_sec]
+def create_samples(data):
+    s = {}
+    second = 0
+    for i in range(0, len(data), sample_rate):
+        s[second] = data[i:i+sample_rate]
+        second+=1
 
-print "EMPTY DICT:", section_dict
-print "LEN SECTION DICT: ", len(section_dict)
+    return s
 
-i = 0
-for j in range(0, len(data_lr_avg), rate):
-    section_dict[i] = data_lr_avg[j:(j+rate)]
-    i += 1
 
-print "dict 0: ", len(section_dict[0]), section_dict[0][0:10]
-print "dict 1: ", len(section_dict[1]), section_dict[1][0:10]
+# Process samples by second
+# INPUT: Dictionary of seconds as keys and matrix of L R raw audio data as values
+# OUTPUT: Returns dictionary of key(frequency) value(time) pairs
+def process_second(s, num):
+    d = {}
+    for i in range(len(s)):  # for second in song
+        mono = channel_avg(s[i])
+        split = split_samples(mono, num)
+        fft = fourier(split)
+        max = max_freq(fft, i)
 
- # ===========================================
-for i in range(0, len(section_dict)):  # WHOLE SONG # i is sample number, s1, s2, etc.
+        if max[0] in d.keys() is not None:   # if dict key exists, append
+            d[max[0]].append(max[1])  # dict_key is max freq and dict_value is time
+        else:  # else create dict key and assign value
+            d[max[0]] = max[1]
+ 
+    return d
+
+
+# =================================================================
+
+# Average Left and right audio channels
+# INPUT: M x 2 matrix, list of 2-element lists (L and R channels)
+# OUTPUT: M x 1 matrix, list
+def channel_avg(raw_data_matrix):
+    data_channel_avg = []
+    for i in range(len(raw_data_matrix)):
+        average = (raw_data_matrix[i][0] + raw_data_matrix[i][1]) / 2
+        data_channel_avg.append(average)
+
+    return data_channel_avg
+
+
+# Split each second into x samples
+# INPUT: list with length of sampling rate
+# OUTPUT: list with length of number of indicated samples per second
+def split_samples(mono_data, num_samp_sec):
+    elements_per_sample = sample_rate/num_samp_sec
     sample = []
-    print "LEN SECTION DICT:", i, " = ", len(section_dict[i])
-    for j in range(0, len(section_dict[i]), (rate/num_samp_sec)):
-        # print j
-        # print section_dict[i][j]
-        added_elements = section_dict[i][j:j+(rate/num_samp_sec)]
+    for i in range(0, len(mono_data), (elements_per_sample)):
+        added_elements = mono_data[i:i+(elements_per_sample)]
         sample_avg = sum(added_elements)/len(added_elements)
-        sample.append(section_dict[i][j])
+        sample.append(sample_avg)
 
-    print "SAMPLE: ", sample
-    print "SAMPLE LENGTH:", len(sample), "SHOULD EQUAL", num_samp_sec
+    return sample
 
 
-    fft_data = np.fft.fft(sample)  # Compute the one-dimensional discrete Fourier Transform. (Returns complex values)
-    print "FFT DATA:", fft_data
-
+# Compute the one-dimensional discrete Fourier Transform
+# INPUT: list with length of number of samples per second
+# OUTPUT: list of real values len of num samples per second
+def fourier(sample):
+    fft_data = np.fft.fft(sample)  # Returns real and complex value pairs
     fft_abs = abs(fft_data)  # Taking abs value to get distance from zero (amplitude of frequency)
-    print "FFT ABS: ", fft_abs
+    return fft_abs
+
+
+# Add max freq in a given second-long sample to time-freq table
+# INPUT: Dict and list of fourier transform results, with len of num samples per second
+# OUTPUT: None, adds key-value pair to dictionary
+def max_freq(fft_abs, sec):
     max_fft_abs = max(fft_abs)
-    print "MAX FFT ABS:", max(fft_abs)
+    dict_value = None
+    for i in range(len(fft_abs)):  # Need to find which 1/10th of sec highest freq occurs
+        if fft_abs[i] == max_fft_abs:
+            tenth = float(i)/float(10)
+            dict_value = float(sec) + tenth # !!!!!!!!!!!!!!!!!!!!!!!!!! NEED TO TRACK SECOND
 
-    dict_key = max_fft_abs
-    print "DICT KEY: ", dict_key
-
-    if dict_key in d.keys() is not None:   # if dict key exists, append
-        d[dict_key].append(i)  # k is frequency(key) and i is sample number(value)
-    else:  # else create dict key and assign value
-        d[dict_key] = i
-
-
-print "DICTIONARY: ", d
-
-test = abs(np.fft.fft(data_lr_avg[0:rate]))
-print "COMPARE - FIRST SEC: ", sum(test)/len(test)
-
-
- # ===========================================
-
-# data_lr_avg_sample = []
-# samples_sec = rate/10
-# for i in range(0, len(data_lr_avg), samples_sec):
-#     sample = data_lr_avg[i:i+samples_sec]
-
-#     sample_avg = sum(sample)/len(sample)
-#     data_lr_avg_sample.append(sample_avg)
-
-# data_lr_avg_sample = []
-# samples_sec = rate/10
-# for i in range(0, len(data_lr_avg), samples_sec):
-#     sample = data_lr_avg[i:i+samples_sec]
-
-#     sample_avg = sum(sample)/len(sample)
-#     data_lr_avg_sample.append(sample_avg)
-
-# print "DATA LR AVG SAMPLE: ", data_lr_avg_sample
-# print "DATA LR AVG SAMPLE LEN:", len(data_lr_avg_sample)
-
-# fft_data = np.fft.fft(data_lr_avg_sample)  # Compute the one-dimensional discrete Fourier Transform. (Returns complex values)
-# print "FFT DATA:", fft_data
-
-# fft_plot = abs(fft_data)  # Taking abs value to get distance from zero (amplitude of frequency)
-# print "FFT PLOT: ", fft_plot
-# http://www.lomont.org/Software/Misc/FFT/SimpleFFT.pdf
-
-# print 'LENGTH OF DATA PLOT: ', len(fft_plot)
-# print "DATA TYPE: ", type(fft_plot)
-# print "ELEMENT DATA TYPE: ", fft_plot.dtype
-
-
-# # t = np.linspace(0,)
-# # # https://sites.google.com/site/haskell102/home/frequency-analysis-of-audio-file-with-python-numpy-scipy
-# plt.plot(test)
-# plt.show()
+    dict_key = round(max_fft_abs, 2) # Round freq to 2 decimal places
+    return (dict_key, dict_value)
 
 
 
-
-# pylab.specgram(fft_plot_sample)
+# Main method 
+a = read_audio('Hiphopopotamus.wav')
+s = create_samples(a)
+p = process_second(s, 10)
