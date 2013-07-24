fe4f340903e41ec84570c59e94bab58f3dcb8b84
Hackbright student <user@mini-005.(none)>
2013-07-23 17:42:22 -0700

    updated sampling

--- freq_plot.py
+++ freq_plot.py
@@ -3,6 +3,7 @@
 
 import scipy.io.wavfile
 import numpy as np
+# import math.floor as floor
 
 
 sample_rate = 44100
@@ -13,15 +14,18 @@ sample_rate = 44100
 # OUTPUT: Sets sample rate of wav file, Returns data read from wav file (numpy array of integers)
 def read_audio(file):
     rate, data = scipy.io.wavfile.read(file)  # Return the sample rate (in samples/sec) and data from a WAV file
-    sample_rate = rate
+    print "RATE: ", rate
     return data
 
 
-def create_samples(data):
+# num_keys is number of dictionary entries per second
+def create_samples(data, num_keys):
     s = {}
     second = 0
-    for i in range(0, len(data), sample_rate):
-        s[second] = data[i:i+sample_rate]
+    num_secs = round(len(data)/sample_rate, 0)
+    num_keys_sec = sample_rate/num_keys
+    for i in range(0, int(num_secs*sample_rate), num_keys_sec):
+        s[second] = data[i:i+num_keys_sec]
         second += 1
 
     return s
@@ -30,13 +34,16 @@ def create_samples(data):
 # Process samples by second
 # INPUT: Dictionary of seconds as keys and matrix of L R raw audio data as values
 # OUTPUT: Returns dictionary of key(frequency) value(time) pairs
-def process_second(s, num):
+def process_second(s, num):  #, num_samp_sec):
     d = {}
+    # second = float(0)
+
     for i in range(len(s)):  # for second in song
         mono = channel_avg(s[i])
         split = split_samples(mono, num)
         fft = fourier(split)
         max = max_freq(fft, i, num)
+        # second += float(1/num_samp_sec)
 
         if max[0] in d.keys() is not None:   # if dict key exists, append
             d[max[0]].append(max[1])  # dict_key is max freq and dict_value is time
@@ -60,10 +67,10 @@ def channel_avg(raw_data_matrix):
     return data_channel_avg
 
 
-# Split each second into x samples
+# Split each second into x samples !!!!!!!!! SHOULD TAKE HIGHEST INSTEAD OF AVG?
 # INPUT: list with length of sampling rate
 # OUTPUT: list with length of number of indicated samples per second
-def split_samples(mono_data, num_samp_sec):
+def split_samples(mono_data, num_samp_sec):  # num samp sec is 20 if two samples per sec from create samples
     elements_per_sample = sample_rate/num_samp_sec
     sample = []
     for i in range(0, len(mono_data), (elements_per_sample)):
@@ -106,6 +113,8 @@ def compare(d1, d2, consec, err):  # start testing err value 10
     i = 0
     j = 0
     time_offset = None
+    prev_match = False
+
     while (i < len(d1)) and (j < len(d2)):
         print "I: ", i
         print "J: ", j
@@ -120,19 +129,25 @@ def compare(d1, d2, consec, err):  # start testing err value 10
 
         if ((freq_delta <= err) and ((time_delta-time_offset) <= 1.0)):  # if potential match (freq err is < given and time offset aligns)
             print "FREQ DELTA ERR SMALL AND TIME OFFSET MATCH"
-  
-            time_offset = time_delta  # abs not necessary?
+
+            time_offset = time_delta
             print 'TIME OFFSET: ', time_offset
             print ""
             i += 1  # go to next index to check if next items match
             j += 1
+            prev_match = True
+            print "PREV MATCH: ", prev_match
 
         else:  # reset d2 counter and time offest if match not found
             print 'MATCH NOT FOUND, INCREASE I, RESET J AND OFFSET'
-            print ""
-            i += 1
+            if prev_match is False:
+                i += 1
+            
             j = 0
             time_offset = None
+            prev_match = False
+            print "PREV MATCH: ", prev_match
+            print ""
 
         if j == consec:
             print consec, "CONSECUTIVE MATCHES FOUND"
@@ -142,18 +157,22 @@ def compare(d1, d2, consec, err):  # start testing err value 10
     return False
 
 
-# Main method
+# Test
+# Entire file
 a1 = read_audio('Hiphopopotamus.wav')
-a2 = create_samples(a1)
-a3 = process_second(a2, 10)
+# a2 = create_samples(a1, 2)
+# a3 = process_second(a2, 10)
 
-b1 = read_audio('Hiphopopotamus.wav')
-b2 = b1[400000:700000]
-b3 = create_samples(b2)
-b4 = process_second(b3, 10)
+# Portion of file to compare
+b1 = a1[0:44100*2]
+b2 = create_samples(b1, 2)
+b3 = process_second(b2, 10)
 
-# d1 = a3[0:5]
-# d2 = b4[0:3]
+c1 = a1[0:700000]
+c2 = create_samples(c1, 2)
+c3 = process_second(c2, 10)
 
-# compare(a3, b4, 3, 300)
+
+
+# compare(a3, b3, 3, 300)
 
