ae1b4ae40a33431e2ed0c5f8a6dc4a55ebc4438b
allison nicole <allisonnicole@allisons-MacBook-Pro.local>
2013-08-16 18:41:24 -0700

    working copy of alignment

--- alignment_by_row_channels.py
+++ alignment_by_row_channels.py
@@ -1,12 +1,8 @@
 import scipy.io.wavfile
-from scipy import stats
 import numpy as np
-from numpy import arange
 from subprocess import call
 import math
-import matplotlib.pyplot as plt
 
-sample_rate = 44100
 
 # Extract audio from video file, save as wav auido file
 # INPUT: Video file
@@ -16,7 +12,6 @@ def extract_audio(dir, video_file):
     audio_output = track_name[0] + "WAV.wav"  # !! CHECK TO SEE IF FILE IS IN UPLOADS DIRECTORY
     output = dir + audio_output
     call(["avconv", "-y", "-i", dir+video_file, "-vn", "-ac", "1", "-f", "wav", output])
-    # audio_data = call(["avconv", "-n", "-i", dir+video_file, "-vn", "-f", "wav", output])
     return output
 
 
@@ -24,20 +19,12 @@ def extract_audio(dir, video_file):
 # INPUT: Audio file
 # OUTPUT: Sets sample rate of wav file, Returns data read from wav file (numpy array of integers)
 def read_audio(audio_file):
-    # filename = audio_file.split(".")
-    # print 'FILENAME: ', filename
-    # # output = "./" + filename[0] + "_1." + filename[1] # app
-    # output = "." + filename[1] + "_1." + filename[2] # test
-    # call(["sox", audio_file, output, "channels", "1"])
-    # rate, data = scipy.io.wavfile.read(output)  # Return the sample rate (in samples/sec) and data from a WAV file
     rate, data = scipy.io.wavfile.read(audio_file)  # Return the sample rate (in samples/sec) and data from a WAV file
-    print "RATE: ", rate  # !! RETURN RATE
-    return data
+    return data, rate
 
 
 def make_horiz_bins(data, fft_bin_size, overlap, box_height):
     horiz_bins = {}
-    len_intensities = 0
     # process first sample and set matrix height
     sample_data = data[0:fft_bin_size]  # get data for first sample
     if (len(sample_data) == fft_bin_size):  # if there are enough audio points left to create a full fft bin
@@ -61,6 +48,7 @@ def make_horiz_bins(data, fft_bin_size, overlap, box_height):
                 else:
                     horiz_bins[box_y] = [(intensities[k], x_coord_counter, k)]
         x_coord_counter += 1
+
     return horiz_bins
 
 
@@ -74,48 +62,34 @@ def fourier(sample):  #, overlap):
         r = fft_data[i].real**2
         j = fft_data[i].imag**2
         mag.append(round(math.sqrt(r+j),2))
+
     return mag
 
 
 def make_vert_bins(horiz_bins, box_width):
     boxes = {}
     for key in horiz_bins.keys():
-        # print "key: ", key
-        # print horiz_bins[key]
         for i in range(len(horiz_bins[key])):
             box_x = horiz_bins[key][i][1] / box_width
             if boxes.has_key((box_x,key)):
                 boxes[(box_x,key)].append((horiz_bins[key][i]))
             else:
                 boxes[(box_x,key)] = [(horiz_bins[key][i])]
+
     return boxes
 
 
 def find_bin_max(boxes, maxes_per_box):
     freqs_dict = {}
-
     for key in boxes.keys():
-        # print "key: ", key
         max_intensities = [(1,2,3)]
         for i in range(len(boxes[key])):
-            # print "i: ", i
-            # print "len key: ", len(boxes[key])
-            # print "max intensities", max_intensities
-            # print 'currently evaluated intensity', boxes[key][i][0]
-            # print "min freq of max intensities", min(max_intensities)[0]
-
             if boxes[key][i][0] > min(max_intensities)[0]:
-                # print "len of max intensities list: ", len(max_intensities)
-                # print "num maxes allowed: ", maxes_per_box
-
                 if len(max_intensities) < maxes_per_box:  # add if < number of points per box
                     max_intensities.append(boxes[key][i])
-                    # print max_intensities
-                    # print ""
                 else:  # else add new number and remove min
                     max_intensities.append(boxes[key][i])
                     max_intensities.remove(min(max_intensities))
-                    # print "replacing ", min(max_intensities)      
         for j in range(len(max_intensities)):
             if freqs_dict.has_key(max_intensities[j][2]):
                 freqs_dict[max_intensities[j][2]].append(max_intensities[j][1])
@@ -124,6 +98,7 @@ def find_bin_max(boxes, maxes_per_box):
 
     return freqs_dict
 
+
 def find_freq_pairs(freqs_dict_orig, freqs_dict_sample):
     time_pairs = []
     for key in freqs_dict_sample.keys():  # iterate through freqs in sample
@@ -131,6 +106,7 @@ def find_freq_pairs(freqs_dict_orig, freqs_dict_sample):
             for i in range(len(freqs_dict_sample[key])):  # determine time offset
                 for j in range(len(freqs_dict_orig[key])):
                     time_pairs.append((freqs_dict_sample[key][i], freqs_dict_orig[key][j]))
+
     return time_pairs
 
 
@@ -145,41 +121,49 @@ def find_delay(time_pairs):
     t_diffs_sorted = sorted(t_diffs.items(), key=lambda x: x[1])
     print t_diffs_sorted
     time_delay = t_diffs_sorted[-1][0]
+
     return time_delay
 
 
+# Find time delay between two video files
+def align(video1, video2, dir, fft_bin_size=1024, overlap=0, box_height=512, box_width=43, samples_per_box=7):
+    # Process first file
+    wavfile1 = extract_audio(dir, video1)
+    raw_audio1, rate = read_audio(wavfile1)
+    bins_dict1 = make_horiz_bins(raw_audio1[:44100*120], fft_bin_size, overlap, box_height) #bins, overlap, box height
+    boxes1 = make_vert_bins(bins_dict1, box_width)  # box width
+    ft_dict1 = find_bin_max(boxes1, samples_per_box)  # samples per box
+
+    # Process second file
+    wavfile2 = extract_audio(dir, video2)
+    raw_audio2, rate = read_audio(wavfile2)
+    bins_dict2 = make_horiz_bins(raw_audio2[:44100*60], fft_bin_size, overlap, box_height)
+    boxes2 = make_vert_bins(bins_dict2, box_width)
+    ft_dict2 = find_bin_max(boxes2, samples_per_box)
+
+    # Determie time delay
+    pairs = find_freq_pairs(ft_dict1, ft_dict2)
+    delay = find_delay(pairs)
+    samples_per_sec = float(rate) / float(fft_bin_size)
+    seconds= round(float(delay) / float(samples_per_sec), 4)
+
+    if seconds > 0:
+        return (seconds, 0)
+    else:
+        return (0, abs(seconds))
 
 
 
-
-
+# ======= TEST FILES ==============
 # audio1 = "regina6POgShQ-lC4.mp4"
-# audio2 = "reginaJo2cUWpILMgWAV.wav"
+# # audio2 = "reginaJo2cUWpILMgWAV.wav"
 # audio1 = "Settle2kFaZIKtcn6s.mp4"
 # audio2 = "Settle2d_tj-9_dGog.mp4"
-audio1 = "DanielZ5PPlk53IMY.mp4"
-audio2 = "Daniel08ycq2T_ab4.mp4"
-directory = "./uploads/"
-
-wavfile1 = extract_audio(directory, audio1)
-raw_audio1 = read_audio(wavfile1)
-print raw_audio1
-bins_dict1 = make_horiz_bins(raw_audio1[:44100*120], 1024, 0, 512) #bins, overlap, box height
-boxes1 = make_vert_bins(bins_dict1, 43)  # box width
-ft_dict1 = find_bin_max(boxes1, 7)  # samples per box
-print "created ft dict 1"
-
-wavfile2 = extract_audio(directory, audio2)
-raw_audio2 = read_audio(wavfile2)
-bins_dict2 = make_horiz_bins(raw_audio2[:44100*30], 1024, 0, 512)
-boxes2 = make_vert_bins(bins_dict2, 43)
-ft_dict2 = find_bin_max(boxes2, 7)
-print "created ft dict 1"
-
-pairs = find_freq_pairs(ft_dict1, ft_dict2)
-delay = find_delay(pairs)
-print "delay: ", float(delay)/43
-
+# audio1 = "DanielZ5PPlk53IMY.mp4"
+# audio2 = "Daniel08ycq2T_ab4.mp4"
+# directory = "./uploads/"
+# t = align(audio1, audio2, directory)
+# print t
 
 
 
